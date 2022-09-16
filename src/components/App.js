0ff9c5783a3d1db999e64227a36020a050fe4329
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-09-16 10:26:58 +0100

    initial attempt with double maps

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,47 @@
-import Map from 'react-map-gl';
-import './App.css';
+import Map from "react-map-gl";
+import "./App.css";
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+// import L from "leaflet"
 
 function App() {
-  return (
-    <div className="App">
-     <Map
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
-      }}
-      style={{width: 600, height: 400}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    mapboxAccessToken={process.env.REACT_APP_MAP_KEY}
-    />
-    </div>
-  );
+    const position = [51.505, -0.09]
+    // var map = L.map("map", {
+    //     center: [51.505, -0.09],
+    //     zoom: 13,
+    // });
+    return (
+        <>
+            <div className="App">
+                <Map
+                    initialViewState={{
+                        longitude: -122.4,
+                        latitude: 37.8,
+                        zoom: 14,
+                    }}
+                    style={{ width: 600, height: 400 }}
+                    mapStyle="mapbox://styles/mapbox/streets-v9"
+                    mapboxAccessToken={process.env.REACT_APP_MAP_KEY}
+                />
+            </div>
+            <div id="map">
+                <MapContainer
+                    center={position}
+                    zoom={14}
+                    scrollWheelZoom={false}
+                >
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+{/*                     <Marker position={position}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker> */}
+                </MapContainer>
+            </div>
+        </>
+    );
 }
 
 export default App;
