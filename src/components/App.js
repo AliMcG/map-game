a32009d4594247eb0a168a73518bd99705629ac9
TomBirbeck <tombirbeck47@gmail.com>
2022-09-16 14:43:01 +0100

    ap clickable

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
-import Map from "react-map-gl";
-import "./App.css";
-import { MapContainer, TileLayer, useMap, Popup, Marker } from "react-leaflet";
-import { useState, useEffect } from "react";
+import Map from 'react-map-gl';
+import './App.css';
+import {
+  MapContainer,
+  TileLayer,
+  useMapEvents,
+  Popup,
+  Marker,
+} from 'react-leaflet';
+import { useState, useEffect } from 'react';
 // import L from "leaflet"
 
 function App() {
@@ -9,14 +15,31 @@ function App() {
   const [long, setLong] = useState();
   const [lat, setLat] = useState();
 
-  const [position, setPosition] = useState([0, 0]);
-  console.log("position", position);
+  function LocationMarker() {
+    const [position, setPosition] = useState(null);
+    const map = useMapEvents({
+      click(e) {
+        setPosition(e.latlng);
+        console.log(e.latlng);
+      },
+      locationfound(e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      },
+    });
+
+    return position === null ? null : (
+      <Marker position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    );
+  }
 
   useEffect(() => {
     getRandomCoords();
   }, []);
   async function getRandomCoords() {
-    const url = "https://api.3geonames.org/?randomland=yes&json=1";
+    const url = 'https://api.3geonames.org/?randomland=yes&json=1';
     const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
     console.log(response);
     const data = await response.json();
@@ -27,11 +50,11 @@ function App() {
   }
   return (
     <>
-      <div className="App">
+      <div className='App'>
         {long && (
           <img
             src={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/${long},${lat},8,0/300x300?access_token=${staticMapApi}`}
-            alt="map"
+            alt='map'
           />
         )}
         {/* <Map
@@ -51,20 +74,21 @@ function App() {
 
         {long && (
           <MapContainer
-            id="map"
-            center={position}
+            id='map'
+            center={[0, 0]}
             zoom={2}
             scrollWheelZoom={false}
           >
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
             />
-            <Marker position={position}>
+            <Marker position={[lat, long]}>
               <Popup>
                 A pretty CSS3 popup. <br /> Easily customizable.
               </Popup>
             </Marker>
+            <LocationMarker />
           </MapContainer>
         )}
       </div>
