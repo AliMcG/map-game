af160e0cd94dfe9e7a15e04f96992fe439ae6ad7
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-09-16 10:08:26 +0100

    map-gl shows a static map tile

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
-
+import Map from 'react-map-gl';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-   
+     <Map
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14
+      }}
+      style={{width: 600, height: 400}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    mapboxAccessToken={process.env.REACT_APP_MAP_KEY}
+    />
     </div>
   );
 }
