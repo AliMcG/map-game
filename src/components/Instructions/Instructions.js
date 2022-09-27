70ab51d577c1eae74fb185bce0ce1fa6de8ba817
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-09-27 19:50:14 +0100

    add instructions box placeholder

---
+++ src/components/Instructions/Instructions.js
@@ -0,0 +1,11 @@
+export default function Instructions() {
+    return (
+        <div className="instructions">
+            <ul className="instructions-list">
+                <li className="instruction-list-item">Step 1:</li>
+                <li>Step 2:</li>
+                <li>Step 3:</li>
+            </ul>
+        </div>
+    );
+}
