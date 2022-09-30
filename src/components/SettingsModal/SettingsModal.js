1b229c42d93d1201fbe3a5c971265006fce057b9
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-09-30 22:29:05 +0100

    making difficulty settings into a form

--- src/components/SettingsModal/SettingsModal.js
+++ src/components/SettingsModal/SettingsModal.js
@@ -8,25 +8,52 @@ function SettingsModal({ showModal, toggleModal }) {
                 <div className="settings-modal">
                     <form>
                         <p>Settings</p>
+                        <label for="tile zoom">Tile zoom</label>
                         <input
+                            // class="input-row"
                             type="number"
                             placeholder="tile zoom"
                             min="1"
                             max="20"
+                            name="tile zoom"
+                            id="tile-zoom"
                         />
+                        <br />
+                        {/* <input
+                            type="range"
+                            name="tile zoom"
+                            min="1"
+                            max="20"
+                            placeholder="tile zoom"
+                        ></input><label for="tile zoom">Tile zoom(between 1 and 20):</label> */}
+                        <label for="mminimum guess radius">
+                            Mimimum guess radius
+                        </label>
                         <input
+                            // class="input-row"
                             type="number"
                             placeholder="minimum guess radius"
                             min="10"
                             max="1000"
+                            name="minimum guess radius"
                         />
-                        <input type="time" placeholder="maximum time" />
+                        <br />
+                        <label for="maximum time">Time limit</label>
                         <input
+                            type="time"
+                            placeholder="maximum time"
+                            name="maximum time"
+                        />
+                        <br />
+                        <input
+                            class="input-row"
                             type="text"
                             placeholder="some other difficulty setting..."
                         />
-                        <button onClick={toggleModal}>Close</button>
-                        <input type="reset" />
+                        <input class="input-row" type="reset" />
+                        <button class="input-row" onClick={toggleModal}>
+                            Close
+                        </button>
                     </form>
                 </div>
             )}
