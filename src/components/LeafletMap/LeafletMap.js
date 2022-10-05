94a1039caaadffb99ecb26d23c0315b1a2d84f1e
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-10-05 22:20:48 +0100

    updated minDistance correctly applies when making a guess

--- src/components/LeafletMap/LeafletMap.js
+++ src/components/LeafletMap/LeafletMap.js
@@ -74,7 +74,7 @@ function LeafletMap({
     console.log('long1: ', lon1, 'Lon2: ', lon2);
     console.log('distance: ', d);
     setDistance(d.toFixed(1));
-    if (d < 100) {
+    if (d < difficulty.minDistance) {
       setAnswer(true);
     }
   }
