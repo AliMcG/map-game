8640c2a758271002008201d54df48fee31ec7cf7
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-10-07 19:05:37 +0100

    Tidy up settings modal inputs and rows

--- src/components/SettingsModal/SettingsModal.js
+++ src/components/SettingsModal/SettingsModal.js
@@ -12,62 +12,68 @@ function SettingsModal({
             <div className="settings-modal">
                 <form>
                     <p>Settings</p>
-                    <label htmlFor="tile zoom">Tile zoom: </label>
-                    <input
-                        // class="input-row"
-                        type="number"
-                        placeholder="tile zoom"
-                        min="6"
-                        value={difficulty.tileZoom}
-                        max="10"
-                        name="tile zoom"
-                        id="tile-zoom"
-                        onChange={(e) => {
-                            changeTileZoom(e.target.value);
-                        }}
-                    />
-                    <br />
-                    <label htmlFor="minimum guess radius">
-                        Mimimum guess radius (km):{" "}
-                    </label>
-                    <input
-                        // class="input-row"
-                        type="number"
-                        placeholder="minimum guess radius"
-                        min="10"
-                        value={difficulty.minDistance}
-                        max="1000"
-                        name="minimum guess radius"
-                        onChange={(e) => {
-                            changeMinDistance(e.target.value);
-                        }}
-                    />
-                    <br />
-                    <label htmlFor="maximum time">Time limit: </label>
-                    <input
-                        type="number"
-                        placeholder="maximum time"
-                        name="maximum time"
-                        value={difficulty.timeLimit}
-                        onChange={(e) => {
-                            changeTimeLimit(e.target.value);
-                        }}
-                    />
-                    <br />
-                    <label htmlFor="max guesses">
-                        Maximum no. of guesses:{" "}
-                    </label>
-                    <input
-                        // className="input-row"
-                        type="number"
-                        placeholder="maximum no. of guesses"
-                        name="max guesses"
-                        min="1"
-                        value={difficulty.maxGuesses}
-                        onChange={(e) => {
-                            changeMaxGuesses(e.target.value);
-                        }}
-                    />
+                    <div className="input-row">
+                        <label htmlFor="tile zoom">Tile zoom: </label>
+                        <input
+                            className="input-field"
+                            type="number"
+                            placeholder="tile zoom"
+                            min="6"
+                            value={difficulty.tileZoom}
+                            max="10"
+                            name="tile zoom"
+                            id="tile-zoom"
+                            onChange={(e) => {
+                                changeTileZoom(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className="input-row">
+                        <label htmlFor="minimum guess radius">
+                            Mimimum guess radius (km):{" "}
+                        </label>
+                        <input
+                            className="input-field"
+                            type="number"
+                            placeholder="minimum guess radius"
+                            min="10"
+                            value={difficulty.minDistance}
+                            max="1000"
+                            name="minimum guess radius"
+                            onChange={(e) => {
+                                changeMinDistance(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className="input-row">
+                        <label htmlFor="maximum time">Time limit: </label>
+                        <input
+                            className="input-field"
+                            type="number"
+                            placeholder="maximum time"
+                            name="maximum time"
+                            value={difficulty.timeLimit}
+                            onChange={(e) => {
+                                changeTimeLimit(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className="input-row">
+                        <label htmlFor="max guesses">
+                            Maximum no. of guesses:{" "}
+                        </label>
+                        <input
+                            className="input-field"
+                            type="number"
+                            placeholder="maximum no. of guesses"
+                            name="max guesses"
+                            min="1"
+                            value={difficulty.maxGuesses}
+                            onChange={(e) => {
+                                changeMaxGuesses(e.target.value);
+                            }}
+                        />
+                    </div>
                     {/* TODO: Apply button may carry out all the changes once, instead of the state updating instantly onChange  */}
                     <button type="submit">Apply</button>
                     <input className="input-row" type="reset" />
