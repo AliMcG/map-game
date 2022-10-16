160ccf6647167899937e5c2a4e52c2d791da3d21
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-10-16 18:58:11 +0100

    rename testGuesses to guesses, add example scoreboard structure, add updater func for scoreboard

--- src/hooks/useLocalScoreboard.js
+++ src/hooks/useLocalScoreboard.js
@@ -2,12 +2,39 @@ import { useState } from "react";
 import React from "react";
 
 export default function useLocalScoreboard() {
-    function getScoreboard () {
-        let localHighScores = JSON.parse(localStorage.getItem("localHighScores"))
-        console.log("localHighScores:" ,localHighScores);
-        return localHighScores
+    /* 
+    Array of objects of each score entry, ex. 
+    [
+        {
+            score: 222,
+            city: Paris,
+            time(s): 50,
+            guesses: 7,
+            date: time.Now(),
+        },
+        {
+            score: 10,
+            city: Arkhangelsk,
+            time(s): 95,
+            guesses: 11,
+            date: time.Now(),
+        },
+    ]
+    
+    */
+    function getScoreboard() {
+        let localHighScores = JSON.parse(
+            localStorage.getItem("localHighScores")
+        );
+        console.log("localHighScores:", localHighScores);
+        return localHighScores;
+    }
+    function setScoreboard(newScoreObject) {
+        localStorage.setItem(
+            "localHighScores",
+            JSON.stringify([...getScoreboard(), newScoreObject])
+        );
     }
 
-    return getScoreboard
-    
+    return getScoreboard;
 }
